6ce7810c2d3866af706ba29ce945e7a8da330631
ElieBendier <144697860+ElieBendier@users.noreply.github.com>
2024-01-03 16:20:08 +0100

    save before merging

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('languages/index.html.twig');
-    }
+
 }
