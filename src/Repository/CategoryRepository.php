424b058f3c3e93dbfeedd5a2d21f2e1194a5cb30
ElieBendier <144697860+ElieBendier@users.noreply.github.com>
2024-01-03 16:20:45 +0100

    saving before merging dev

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -20,6 +20,4 @@ class CategoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Category::class);
     }
-
-
 }
--- src/Repository/LanguageRepository.php
+++ src/Repository/LanguageRepository.php
@@ -20,5 +20,4 @@ class LanguageRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Language::class);
     }
-
 }
