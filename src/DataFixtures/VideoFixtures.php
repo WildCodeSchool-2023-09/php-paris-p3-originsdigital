99214882f7b93ae863c600171b81955fdc42e6ff
NicoDestephen <ndestephen@gmail.com>
2024-01-05 14:59:16 +0100

    [UPDATE] Videofixtures

--- src/DataFixtures/VideoFixtures.php
+++ src/DataFixtures/VideoFixtures.php
@@ -19,66 +19,94 @@ class VideoFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
-        $firstLesson = new Video();
-        $firstLesson  ->setTitle("Apprendre #Symfony 6 - Installation et configuration")
-            ->setThumbnail("/uploads/thumbnails/test1.jpg")
-            ->setDescription("Installation et configuration")
-            ->setVideo("/uploads/videos/test1.mp4")
-            ->setCategory($this->getReference("PHPLesson"));
-        $slug = $this->slugger->slug($firstLesson->getTitle());
-        $firstLesson->setSlug($slug);
-        $manager->persist($firstLesson);
-        $secondLesson = new Video();
-        $secondLesson  ->setTitle("Apprendre #Symfony 6 - Notre première page")
-            ->setThumbnail("/uploads/thumbnails/test2.jpg")
-            ->setDescription("Notre première page")
-            ->setVideo("/uploads/videos/test2.mp4")
-            ->setCategory($this->getReference("PHPLesson"));
-        $slug = $this->slugger->slug($secondLesson->getTitle());
-        $secondLesson->setSlug($slug);
-        $manager->persist($secondLesson);
-        $thirdLesson = new Video();
-        $thirdLesson  ->setTitle("Apprendre #Symfony 6 - Twig & Symfony")
-            ->setThumbnail("/uploads/thumbnails/test3.jpg")
-            ->setDescription("Twig & Symfony")
-            ->setVideo("/uploads/videos/test3.mp4")
-            ->setCategory($this->getReference("PHPLesson"));
-        $slug = $this->slugger->slug($thirdLesson->getTitle());
-        $thirdLesson->setSlug($slug);
-        $manager->persist($thirdLesson);
-        $fourthLesson = new Video();
-        $fourthLesson  ->setTitle("Apprendre #Symfony 6 - Notre première entité")
-            ->setThumbnail("/uploads/thumbnails/test4.jpg")
-            ->setDescription("Notre première entité")
-            ->setVideo("/uploads/videos/test4.mp4")
-            ->setCategory($this->getReference("PHPLesson"));
-        $slug = $this->slugger->slug($fourthLesson->getTitle());
-        $fourthLesson->setSlug($slug);
-        $manager->persist($fourthLesson);
-        $fifthLesson = new Video();
-        $fifthLesson  ->setTitle("Apprendre #Symfony 6 - Validation des entités")
-            ->setThumbnail("/uploads/thumbnails/test5.jpg")
-            ->setDescription("Validation des entités")
-            ->setVideo("/uploads/videos/test5.mp4")
-            ->setCategory($this->getReference("PHPLesson"));
-        $slug = $this->slugger->slug($fifthLesson->getTitle());
-        $fifthLesson->setSlug($slug);
-        $manager->persist($fifthLesson);
-        $sixthLesson = new Video();
-        $sixthLesson  ->setTitle("Apprendre #Symfony 6 - Les Fixtures")
-            ->setThumbnail("/uploads/thumbnails/test6.jpg")
-            ->setDescription(" Les Fixtures")
-            ->setVideo("/uploads/videos/test6.mp4")
-            ->setCategory($this->getReference("PHPLesson"));
-        $slug = $this->slugger->slug($sixthLesson->getTitle());
-        $sixthLesson->setSlug($slug);
-        $manager->persist($sixthLesson);
+        $videoData = [
+                    'title' => [
+                                            'Live Coding 1',
+                                            'Live Coding 2',
+                                            'Live Coding 3',
+                                            'Live Coding 4',
+                                            'Tutoriel 1',
+                                            'Tutoriel 2',
+                                            'Tutoriel 3',
+                                            'Tutoriel 4',
+                                            "Apprendre #Symfony 6 - Installation et configuration",
+                                            "Apprendre #Symfony 6 - Notre première page",
+                                            "Apprendre #Symfony 6 - Twig & Symfony",
+                                            "Apprendre #Symfony 6 - Notre première entité",
+                                            "Apprendre #Symfony 6 - Validation des entités",
+                                            "Apprendre #Symfony 6 - Les Fixtures",],
+                    'thumbnail' => [
+                                            '/uploads/thumbnails/test1.jpg',
+                                            '/uploads/thumbnails/test2.jpg',
+                                            '/uploads/thumbnails/test3.jpg',
+                                            '/uploads/thumbnails/test4.jpg',
+                                            '/uploads/thumbnails/test1.jpg',
+                                            '/uploads/thumbnails/test2.jpg',
+                                            '/uploads/thumbnails/test3.jpg',
+                                            '/uploads/thumbnails/test4.jpg',
+                                            '/uploads/thumbnails/test1.jpg',
+                                            '/uploads/thumbnails/test2.jpg',
+                                            '/uploads/thumbnails/test3.jpg',
+                                            '/uploads/thumbnails/test4.jpg',
+                                            '/uploads/thumbnails/test5.jpg',
+                                            '/uploads/thumbnails/test6.jpg',],
+                    'description' => [
+                                            'live coding 1',
+                                            'live coding 2',
+                                            'live coding 3',
+                                            'live coding 4',
+                                            'tuto 1',
+                                            'tuto 2',
+                                            'tuto 3',
+                                            'tuto 4',
+                                            'Installation et configuration',
+                                            'Notre première page',
+                                            'Twig & Symfony',
+                                            'Notre première entité',
+                                            'Validation des entités',
+                                            'Les Fixtures',],
+                    'video' => [
+                                            '/uploads/videos/test1.mp4',
+                                            '/uploads/videos/test2.mp4',
+                                            '/uploads/videos/test3.mp4',
+                                            '/uploads/videos/test4.mp4',
+                                            '/uploads/videos/test1.mp4',
+                                            '/uploads/videos/test2.mp4',
+                                            '/uploads/videos/test3.mp4',
+                                            '/uploads/videos/test4.mp4',
+                                            '/uploads/videos/test1.mp4',
+                                            '/uploads/videos/test2.mp4',
+                                            '/uploads/videos/test3.mp4',
+                                            '/uploads/videos/test4.mp4',
+                                            '/uploads/videos/test5.mp4',
+                                            '/uploads/videos/test6.mp4',],
+                    'categoryReference' => [
+                                            'PHPLiveCoding', 'PHPLiveCoding', 'PHPLiveCoding', 'PHPLiveCoding',
+                                            'PHPTutoriel','PHPTutoriel','PHPTutoriel','PHPTutoriel',
+                                            'PHPLesson','PHPLesson','PHPLesson','PHPLesson','PHPLesson','PHPLesson',],
+                ];
+        $i = 0;
+        foreach ($videoData['title'] as $title) {
+            $video = new Video();
+            $video->setTitle($videoData['title'][$i]);
+            $video->setThumbnail($videoData['thumbnail'][$i]);
+            $video->setDescription($videoData['description'][$i]);
+            $video->setVideo($videoData['video'][$i]);
+            $video->setCategory($this->getReference($videoData['categoryReference'][$i]));
+            $i = $i + 1;
+            $slug = $this->slugger->slug($video->getTitle());
+            $video->setSlug($slug);
+
+            $manager->persist($video);
+        }
+
         $manager->flush();
     }
+
     public function getDependencies()
     {
         return [
-        CategoryFixtures::class,
+            CategoryFixtures::class,
         ];
     }
 }
