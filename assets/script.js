5111434a6be8f6dd719f37dd84511c1994d14f71
2z75 <zinedine.chader@gmail.com>
2024-01-07 12:57:23 +0100

    add preview thumbnail and video

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,38 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var thumbnailFileInput = document.getElementById('upload_video_thumbnailFile_file');
+    var videoFileInput = document.getElementById('upload_video_videoFile_file');
+    var thumbnailPreview = document.getElementById('thumbnailPreview');
+    var videoPreview = document.getElementById('videoPreview');
 
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     const imageInput = document.querySelector('thumbnailFile');
-    //     const imagePreview = document.querySelector('#image-preview');
+    if (thumbnailFileInput && videoFileInput) {
+        thumbnailFileInput.addEventListener('change', function () {
+            readURL(this, thumbnailPreview);
+        });
 
-    //     imageInput.addEventListener('change', function() {
-    //         const file = this.files[0];
+        videoFileInput.addEventListener('change', function () {
+            readURL(this, videoPreview);
+        });
 
-    //         if (file) {
-    //             const reader = new FileReader();
+        function readURL(input, preview) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
 
-    //             reader.onload = function(e) {
-    //                 imagePreview.src = e.target.result;
-    //                 imagePreview.style.display = 'block';
-    //             };
+                reader.onload = function (e) {
+                    // Vérifiez le type MIME du fichier
+                    if (input.files[0].type.startsWith('image')) {
+                        // Afficher l'aperçu en tant qu'image
+                        preview.innerHTML = '<img src="' + e.target.result + '" alt="Thumbnail">';
+                    } else if (input.files[0].type.startsWith('video')) {
+                        // Afficher l'aperçu en tant que vidéo
+                        preview.innerHTML = '<video width="320" height="240" controls><source src="' + e.target.result + '" type="video/mp4"></video>';
+                    } else {
+                        // Type de fichier non pris en charge
+                        console.log('Type de fichier non pris en charge');
+                    }
+                };
 
-    //             reader.readAsDataURL(file);
-    //         } else {
-    //             imagePreview.src = '#';
-    //             imagePreview.style.display = 'none';
-    //         }
-    //     });
-    // });
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    }
+});
--- src/Form/UploadVideoType.php
+++ src/Form/UploadVideoType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Language;
 use App\Entity\Video;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
@@ -14,14 +19,32 @@ class UploadVideoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class, [
+                'attr' => [
+                'placeholder' => 'Titre'
+                ]])
+
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => 'Description'
+                ]])
+
             ->add('thumbnailFile', VichFileType::class, [
                 'label'          => 'Image',
                 'required'       => false,
                 'allow_delete'   => true,
                 'download_uri'   => true,
-        ])
+                'constraints'   => [
+                    new File([
+                        'maxSize'       => '5M',
+                        'mimeTypes'     => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide ({{ types }}).',
+                    ])
+                ],
+            ])
 
             ->add('videoFile', VichFileType::class, [
                 'required'      => false,
@@ -36,7 +59,18 @@ class UploadVideoType extends AbstractType
                         'mimeTypesMessage' => 'Veuillez télécharger un fichier vidéo valide ({{ types }}).',
                     ]),
                 ],
-            ]);
+            ])
+
+            ->add('language', EntityType::class, [
+                'mapped' => false,
+                'class' => Language::class,
+                'choice_label' => 'label'
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'label'
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
