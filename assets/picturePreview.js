b8809a2ddfc64019ad378df19cc4adb403895193
ArnoldMsl <arnold.masselin@gmail.com>
2024-01-07 11:52:14 +0100

    reworked Picturepreview script and form

--- assets/picturePreview.js
+++ assets/picturePreview.js
@@ -1,32 +1,20 @@
 function createThumbnail(sFile) {
-    var oReader = new FileReader();
-    oReader.addEventListener('load', function () {
-        var oImgElement = document.createElement('img');
-        const inputfield = document.getElementById('user_profilepictureFile_file')
-        oImgElement.classList.add('imgPreview')
-        oImgElement.src = this.result;
-        document.getElementById('preview_pictures_field').appendChild(oImgElement);
-        inputfield.style.display = "none";
-    }, false);
+  var oReader = new FileReader();
+  oReader.addEventListener('load', function () {
+    let inputfield = document.querySelector('.file-input-field');
+    inputfield.style.backgroundImage = `url(${oReader.result})`;
+  }, false);
 
-    oReader.readAsDataURL(sFile);
+  oReader.readAsDataURL(sFile);
 }
 
 function changeInputFile(oEvent) {
-    var oInputFile = oEvent.currentTarget;
-    var aFiles = oInputFile.files;
-
-    document.getElementById('preview_pictures_field').innerHTML = '';
-
-    for (var i = 0; i < aFiles.length; i++) {
-        createThumbnail(aFiles[i]);
-    }
+  var oInputFile = oEvent.currentTarget;
+  var aFiles = oInputFile.files;
+  createThumbnail(aFiles[0]);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    var aFileInput = document.forms['user'].getElementById('user_profilepictureFile_file');
-
-    for (var k = 0; k < aFileInput.length; k++) {
-        aFileInput[k].addEventListener('change', changeInputFile, false);
-    }
+  var aFileInput = document.forms['user'].querySelectorAll('[type=file]');
+  aFileInput[0].addEventListener('change', changeInputFile, false);
 });
