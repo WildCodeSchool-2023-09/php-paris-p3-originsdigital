6eb5a87de3fd5579930815a75893b00da3e2c8b5
ArnoldMsl <arnold.masselin@gmail.com>
2024-01-08 12:39:40 +0100

    added fixtures with correct password hash

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -10,27 +10,28 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // Fixture d'un utilisateur connecté, seuls les champs obligatoires pour ce type de profil sont renseignés//
+        // Fixture d'un utilisateur connecté, seuls les champs obligatoires pour ce type de profil sont renseignés.
+        //Mot de passe : '1234'//
         $user = new User();
         $user->setUsername('USER');
         $user->setEmail('user@test.com');
-        $user->setPassword('1234');
+        $user->setPassword('$2y$13$tUkFUXy5bz8e.bhLkBhE0.pf/QUvEIDAPkyOWCwJAXhmIkF93bJLK');
         $user->setRoles(['ROLE_USER']);
         $manager->persist($user);
 
-        //Fixture d'un utilisateur premium, seuls les chammps obligatoires sont renseignés//
+        //Fixture d'un premium, seuls les chammps obligatoires sont renseignés. Mot de passe : '1234'//
         $premium = new User();
         $premium->setUsername('PREMIUM');
         $premium->setEmail('premium@test.com');
-        $premium->setpassword('1234');
+        $premium->setpassword('$2y$13$tUkFUXy5bz8e.bhLkBhE0.pf/QUvEIDAPkyOWCwJAXhmIkF93bJLK');
         $premium->setRoles(['ROLE_PREMIUM']);
         $manager->persist($premium);
 
-        //Fixture d'un admin, seuls les chammps obligatoires sont renseignés//
+        //Fixture d'un admin, seuls les chammps obligatoires sont renseignés. Mot de passe : '1234'//
         $admin = new User();
         $admin->setUsername('ADMIN');
         $admin->setEmail('admin@test.com');
-        $admin->setPassword('1234');
+        $admin->setPassword('$2y$13$tUkFUXy5bz8e.bhLkBhE0.pf/QUvEIDAPkyOWCwJAXhmIkF93bJLK');
         $admin->setRoles(['ROLE_ADMIN']);
         $manager->persist($admin);
         $manager->flush();
