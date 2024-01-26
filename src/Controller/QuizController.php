11a3c89b691ae1fb6561ae62ec547d0e41eec0d0
NicoDestephen <ndestephen@gmail.com>
2024-01-26 12:01:38 +0100

    sharing with arnold

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Course;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,8 +13,13 @@ class QuizController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
     #[Route('/quiz/{id}', name: 'app_quiz')]
-    public function quiz(Course $course): Response
+    public function quiz(Course $course, Request $request): Response
     {
+        if ($request->isMethod('POST')) {
+            // $userAnswers = $request->request->get('pathInformation');
+
+            return $this->redirectToRoute('app_quiz');
+        }
         return $this->render('Quiz/quiz.html.twig', [
             'course' => $course,
         ]);
