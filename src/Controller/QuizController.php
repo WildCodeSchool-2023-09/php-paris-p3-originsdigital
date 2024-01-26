7370ececc2241c29fccc91a3d04a4d9af70a209f
ArnoldMsl <arnold.masselin@gmail.com>
2024-01-26 11:06:29 +0100

    commit before switch

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Course;
+use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,17 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class QuizController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
-    #[Route('/quiz/{id}', name: 'app_quiz')]
-    public function quiz(Course $course): Response
+    #[Route('/quiz', name: 'app_quiz')]
+    public function firstQuiz(): Response
     {
+        return $this->render('quiz/quiz.html.twig');
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/quiz/{id}', name: 'app_quiz')]
+    public function quiz(int $id, Course $course, CourseRepository $courseRepository): Response
+    {
+        $course = $courseRepository->findOneById(['id' => $id]);
         return $this->render('Quiz/quiz.html.twig', [
             'course' => $course,
         ]);
