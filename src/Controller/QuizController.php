c5109cc5ff2364ddf904b833dc35ee233e94fa83
ArnoldMsl <arnold.masselin@gmail.com>
2024-02-01 15:03:04 +0100

    JSON working

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CourseRepository;
-use App\Repository\QuestionRepository;
+use App\Service\QuizService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -38,46 +38,26 @@ class QuizController extends AbstractController
     }
 
     #[IsGranted('ROLE_USER')]
-    #[Route('/score', name: 'app_score')]
-    public function getPlaylist(Request $request, QuestionRepository $questionRepository): Response
-    {
+    #[Route('/generatePlaylist', name: 'app_generatePlaylist')]
+    public function getPlaylistByQuiz(
+        Request $request,
+        CourseRepository $courseRepository,
+        QuizService $quizService
+    ): Response {
         $data = json_decode($request->getContent(), true);
 
-        $score = 0;
-        $totalQuestions = 0;
-        $question = null;
+        $scoreInPercent = $quizService->calculateScore($data);
 
-        foreach ($data as $questionId => $answerId) {
-            $question = $questionRepository->find($questionId);
-            if ($question && $question->getCourse()) {
-                $totalQuestions++;
-                foreach ($question->getAnswers() as $answer) {
-                    if ($answer->getId() == $answerId && $answer->isIsCorrect()) {
-                        $score += 1;
-                    }
-                }
-            }
-        }
+        $courseId = $courseRepository->findOneByLabel($data['course'])->getId();
 
-        $scoreInPercent = ($totalQuestions > 0) ? ($score / $totalQuestions) * 100 : 0;
-        $course = $questionRepository->findByCourse($question->getCourse());
         $userId = $this->getUser()->getId();
 
         $responseData = [
-            'course' => $course,
+            'courseId' => $courseId,
             'userId' => $userId,
             'scoreInPercent' => $scoreInPercent,
         ];
 
         return new JsonResponse($responseData);
-        // appelle la fonction de zinedine pour générrer la playlist
-        // retourne la playlist suivant les données
-        // la playlist je la retourne dans la fin de la fonction que je peux remettre dans le then du fetch
-
-        // return $this->redirectToRoute('assign_playlist', [
-        //     'userId' => $userId,
-        //     'course' => $course,
-        //     'percentage' => $scoreInPercent,
-        // ]);
     }
 }
---
+++ src/Service/quizService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\QuestionRepository;
+
+class QuizService
+{
+    private QuestionRepository $questionRepository;
+
+    public function __construct(QuestionRepository $questionRepository)
+    {
+        $this->questionRepository = $questionRepository;
+    }
+
+    public function calculateScore(array $data): int
+    {
+        $score = 0;
+        $totalQuestions = 0;
+
+        foreach ($data['result'] as $questionId => $answerId) {
+            $question = $this->questionRepository->findOneById($questionId);
+            if ($question && $question->getCourse()) {
+                $totalQuestions++;
+                foreach ($question->getAnswers() as $answer) {
+                    if ($answer->getId() == $answerId && $answer->isIsCorrect()) {
+                        $score += 1;
+                    }
+                }
+            }
+        }
+        return ($score / $totalQuestions) * 100;
+    }
+}
